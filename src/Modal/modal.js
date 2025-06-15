5b74690bd5e124f5110d1c903dca6d7fc414c277
ZIN MYO LATT <myolatt@ZINs-MacBook-Air.local>
2025-06-15 20:08:24 +0630

    working with form

--- src/Modal/modal.js
+++ src/Modal/modal.js
@@ -2,14 +2,18 @@ import React from 'react';
 import "./modal.css";
 import ReactDOM from 'react-dom';
 
-export default function Modal( { children }) {
-  return (
+export default function Modal( { children, setShowModal }) {
 
+  const closeModal = () => {
+    setShowModal( false);
+  }
+  return (
     ReactDOM.createPortal(
         <div className="modal-backdrop">
           <div className="modal-content">
             {/* modal content comes here */}
               { children }
+              <button type='button' onClick={closeModal}> close </button>
           </div>
         </div>, document.querySelector("#modal") )
   )
